44c09d9eebbbd5b407c658f073c0c12011b42270
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2023-12-31 15:43:57 +0000

    Work

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/get-user/<user-id>")
+@app.route("/get-user<user_id>")
 def get_user(user_id):
     user_data = {
         "user_id": user_id,
-        "name": "jake",
-        "email": "example@example.com"
     }
-
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
     
     return jsonify(user_data), 200
 
